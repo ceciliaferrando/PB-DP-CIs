83d90fe7fb1abe839513ba669fd1796f9b71bfea
Cecilia Ferrando <33758082+ceciliaferrando@users.noreply.github.com>
2022-02-22 12:57:53 +0100

    Update DPCIs-OLS.py

--- DPCIs-OLS.py
+++ DPCIs-OLS.py
@@ -40,43 +40,32 @@ def make_pos_def(x, small_positive=0.1):
 
     return xPSD
 
-
-def generate_ols_data(N, D, bound_beta, gamma, delta):
+def generate_ols_data(N, D, true_beta, gamma, delta, zeta):
     '''
     :param N: number of data points
     :param D: number of dimension
-    :param bound_beta: bound on beta, the ols coefficients
+    :param true_beta: true OLS arameter
     :param gamma: bound on x, the ols data
     :param delta: bound on u, the ols noise
     :return: X, U, Y (generated dataset ), beta (generated coefficient beta)
     '''
-
-    beta = np.random.uniform(bound_beta, bound_beta, D)  # true beta
+    boundY = [-zeta, zeta]
+    beta = np.array([true_beta]*D)
     X = np.random.uniform(-gamma, gamma, (N, D))
+    # uniformly generate N error terms
     U = np.random.uniform(-delta, delta, N)
+    # construct the Y
     Y = np.dot(X, beta) + U
+
+    #discard data points that violate the bound on Y, if any (given bounds are conservative)
+    indices = np.hstack((np.where(Y<boundY[0]), np.where(Y>boundY[1])))
+    X = np.delete(X, indices, axis = 0)
+    Y = np.delete(Y, indices)
 
     return X, U, Y, beta
 
 
-def compute_sensitivity(beta, gamma, D, delta):
-    '''
-    :param beta: the ols coefficients
-    :param gamma: bound on x, the ols data
-    :param D: number of dimension
-    :param delta: bound on u, the ols noise
-    :return: Delta_XX, the global sensitivity on XtX; Delta_XY, the global sensitivity on XtY
-    '''
-
-    Delta_XY = beta * gamma * gamma + 2 * gamma * delta
-    Delta_XX = np.ones((D, D)) * (gamma ** 2) * 2
-    for i_x in range(D):
-        Delta_XX[i_x][i_x] = gamma ** 2
-
-    return Delta_XX, Delta_XY
-
-
-def compute_dp_noise(Delta_XY, Delta_XX, D, eps):
+def compute_dp_noise(D, gamma, zeta, eps):
     '''
     :param gs_XY: global sensitivity computed on XtY
     :param gs_XX: global sensitivity computed on XtX
@@ -84,18 +73,18 @@ def compute_dp_noise(Delta_XY, Delta_XX, D, eps):
     :return: w, the laplace noise to be added on XY term, a D-dim vector
              v, the laplace noise to be added on XX term, a D-D matrix
     '''
-
-    w = np.zeros_like(Delta_XY)
+    Delta_w = D * 2*gamma * zeta
+    w = np.zeros((D,))
     for i_w in range(len(w)):
-        w[i_w] = np.random.laplace(0, Delta_XY[i_w] / eps, 1)
+        w[i_w] = np.random.laplace(0, Delta_w / eps/3, 1)
 
+    Delta_V = D*(D + 1) * gamma**2
     V = np.zeros((D, D))
     for i_x in range(D):
         for j_x in range(D):
             if i_x <= j_x:
-                V[i_x][j_x] = np.random.laplace(0, Delta_XX[i_x][j_x] / eps, 1)
+                V[i_x][j_x] = np.random.laplace(0, Delta_V / eps/3, 1)
                 V[j_x][i_x] = V[i_x][j_x]
-
     return w, V
 
 
@@ -119,7 +108,7 @@ def determine_trial_result(CI_lower, CI_upper, value):
 # CORE FUNCTIONS
 ############################################################################
 
-def private_OLS(X, y, beta_true, args):
+def private_OLS(X, y, args):
     """
     :param X: independent variable data
     :param y: dependent variable data
@@ -129,11 +118,11 @@ def private_OLS(X, y, beta_true, args):
     :return: private estimates of beta, Q, sigma; sensitivity of XtX, sensitivity of XtY
     """
 
-    Delta_XX, Delta_XY = compute_sensitivity(beta_true, args.gamma, args.D, args.delta)
-    w, V = compute_dp_noise(Delta_XY, Delta_XX, args.D, args.eps / 3)
+    w, V = compute_dp_noise(args.D, args.gamma, args.zeta, args.eps)
     XtX = np.dot(X.T, X)
     Xty = np.dot(X.T, y)
 
+    beta_hat = np.dot(inv(XtX), Xty)  # this is the non-private estimate for beta
     beta_hat_priv = np.dot(inv(XtX + V), (Xty + w))
 
     Q_hat = 1 / args.N * XtX
@@ -141,19 +130,21 @@ def private_OLS(X, y, beta_true, args):
     if is_pos_def(Q_hat_priv) == False:
         Q_hat_priv = make_pos_def(Q_hat_priv, small_positive=0.1)
 
+    upper_bound_x, lower_bound_x = args.gamma, -args.gamma
+    upper_bound_y, lower_bound_y = args.zeta, -args.zeta
+    width_term = max((upper_bound_y - np.sum(lower_bound_x * np.abs(beta_hat))) ** 2,
+                     (lower_bound_y - np.sum(upper_bound_x * np.abs(beta_hat))) ** 2)
+    Delta_sigma_sq = 1.0 / (args.N - args.D) * width_term
+    sigma_sq_hat_priv = 1.0 / (args.N - args.D) * np.sum((y - np.dot(X, beta_hat)) ** 2) + \
+                        np.random.laplace(0, Delta_sigma_sq / args.eps / 3)  ### note ###
 
-    upper_bound_y = np.max(y)
-    lower_bound_x_beta = np.abs(np.sum(args.gamma * np.abs(beta_hat_priv)))
-    gf_sigma_sq = 1.0 / (args.N - args.D) * (upper_bound_y + lower_bound_x_beta)
-    noise_sigma = np.random.laplace(0, gf_sigma_sq / args.eps / 3)
-    sigma_sq_hat_priv = 1.0 / (args.N - args.D) * np.sum((y - np.dot(X, beta_hat_priv)) ** 2) + noise_sigma
     if sigma_sq_hat_priv < 0:
         sigma_sq_hat_priv = 0.1
 
-    return (beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv, Delta_XX, Delta_XY)
+    return (beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv)
 
 
-def hybrid_bootstrap_OLS(beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv, Delta_XX, Delta_XY, args):
+def hybrid_bootstrap_OLS(beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv, args):
     """
     :param beta_hat_priv: private estimate of beta
     :param Q_hat_priv: private estimate of Q
@@ -168,12 +159,12 @@ def hybrid_bootstrap_OLS(beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv, Delta_XX,
 
     for b in range(args.num_bootstraps):
 
-        w_star = compute_dp_noise(Delta_XY, Delta_XX, args.D, args.eps / 3)[0]
-        V_star = compute_dp_noise(Delta_XY, Delta_XX, args.D, args.eps / 3)[1]
+        w_star = compute_dp_noise(args.D, args.gamma, args.zeta, args.eps)[0]
+        V_star = compute_dp_noise(args.D, args.gamma, args.zeta, args.eps)[1]
 
         cov_matrix = sigma_sq_hat_priv * Q_hat_priv
-        if is_pos_def(cov_matrix) == False:
-            cov_matrix = make_pos_def(cov_matrix)
+        # if is_pos_def(cov_matrix) == False:
+        #     cov_matrix = make_pos_def(cov_matrix)
         Z_star = np.random.multivariate_normal(np.zeros(args.D), cov_matrix)
 
         Q_hat_star = Q_hat_priv + 1 / args.N * V_star
@@ -191,7 +182,7 @@ def coverage_test_single_trial(args):
     :return: dictionary with results from a single trial
     """
 
-    X, U, y, beta_true = generate_ols_data(args.N, args.D, args.bound_beta, args.gamma, args.delta)
+    X, U, y, beta_true_vec = generate_ols_data(args.N, args.D, args.true_beta, args.gamma, args.delta, args.zeta)
 
     dict_result = defaultdict(dict)
 
@@ -201,31 +192,29 @@ def coverage_test_single_trial(args):
     XtX = np.dot(X.T, X)
     beta_hat = np.dot(inv(XtX), XtY)  # this is the non-private estimate for beta
     sigma_sq_est = 1.0 / (args.N - args.D) * np.sum((y - np.dot(X, beta_hat)) ** 2)  # non-private estimate for sigma
-    dist_std = np.sqrt(1.0 * sigma_sq_est / XtX[args.test_d][args.test_d])  # STD in the distribution used to get CI
+    dist_std = np.sqrt(1.0 * sigma_sq_est / XtX[args.test_d][args.test_d])
 
     CI_lower = beta_hat[args.test_d] - args.z_score * dist_std
     CI_upper = beta_hat[args.test_d] + args.z_score * dist_std
-    dict_result['coverage']['standard'] = determine_trial_result(CI_lower, CI_upper, beta_true[args.test_d])
+    dict_result['coverage']['standard'] = determine_trial_result(CI_lower, CI_upper, beta_true_vec[args.test_d])
     dict_result['ci']['standard'] = (CI_lower, CI_upper)
     dict_result['estimate']['standard'] = beta_hat
 
     # naive private method (Fisher CIs)
 
-    (beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv, Delta_XX, Delta_XY) = private_OLS(X, y, beta_true, args)
+    (beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv) = private_OLS(X, y, args)
 
-    upper_bound_y = np.max(y)
-    lower_bound_x_beta = np.abs(np.sum(args.gamma * np.abs(beta_hat_priv)))
     dist_std = np.sqrt(1.0 * sigma_sq_hat_priv / (args.N * Q_hat_priv[args.test_d][args.test_d]))
 
     CI_lower_priv = beta_hat_priv[args.test_d] - args.z_score * dist_std
     CI_upper_priv = beta_hat_priv[args.test_d] + args.z_score * dist_std
-    dict_result['coverage']['private'] = determine_trial_result(CI_lower_priv, CI_upper_priv, beta_true[args.test_d])
+    dict_result['coverage']['private'] = determine_trial_result(CI_lower_priv, CI_upper_priv, beta_true_vec[args.test_d])
     dict_result['ci']['private'] = (CI_lower_priv, CI_upper_priv)
     dict_result['estimate']['private'] = beta_hat_priv
 
     # hybrid bootstrap private method
 
-    bootstrap_vec = hybrid_bootstrap_OLS(beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv, Delta_XX, Delta_XY, args)
+    bootstrap_vec = hybrid_bootstrap_OLS(beta_hat_priv, Q_hat_priv, sigma_sq_hat_priv, args)
 
     empirical_CIs = np.zeros((args.D, 2))
     for i in range(args.D):
@@ -236,7 +225,7 @@ def coverage_test_single_trial(args):
     CI_upper_bootstrap = empirical_CIs[args.test_d][1]
 
     dict_result['coverage']['bootstrap'] = determine_trial_result(CI_lower_bootstrap, CI_upper_bootstrap,
-                                                                  beta_true[args.test_d])
+                                                                  beta_true_vec[args.test_d])
     dict_result['ci']['bootstrap'] = (CI_lower_bootstrap, CI_upper_bootstrap)
     dict_result['estimate']['bootstrap'] = np.mean(bootstrap_vec, axis=0)
 
@@ -248,11 +237,12 @@ def coverage_test_single_trial(args):
 ######################################################################
 np.random.seed(21)
 parser = argparse.ArgumentParser()
-parser.add_argument('--num_bootstraps', type=int, default=100, help='Number of experiments to compute quantile')
+parser.add_argument('--num_bootstraps', type=int, default=500, help='Number of experiments to compute quantile')
 parser.add_argument('--eps', type=float, default=5, help='privacy threshold args.epsilon')
 parser.add_argument('--gamma', type=int, default=5, help='bound on x')
 parser.add_argument('--delta', type=float, default=10, help='bound on u')
-parser.add_argument('--bound_beta', type=float, default=10.0, help='bound on beta')
+parser.add_argument('--zeta', type=float, default=150, help='bound on Y')
+parser.add_argument('--true_beta', type=float, default=10.0, help='true beta')
 parser.add_argument('--z_score', type=float, default=1.96,
                     help='z score associated with confidence level')  # 1.96 for 95% confidence level
 parser.add_argument('--num_trials', type=int, default=1000, help='Number of experiments to compute coverage ratio')
@@ -266,7 +256,9 @@ print(args)
 
 if __name__ == "__main__":
 
-    data_dir = 'data022122/'
+    np.random.seed(451)
+
+    data_dir = 'data022122c/'
 
     save_dir = data_dir
     if not os.path.exists(save_dir):
@@ -285,14 +277,10 @@ if __name__ == "__main__":
         trial_results = np.zeros((args.num_trials,2))
         trial_results_naive = np.zeros((args.num_trials,2))
         trial_results_standard = np.zeros((args.num_trials,2))
-
         trial_widths = np.zeros((args.num_trials, 2))
         trial_widths_naive = np.zeros((args.num_trials, 2))
         trial_widths_standard = np.zeros((args.num_trials, 2))
 
-
-        #print('\n\n')
-        #print(f'Test case {N_idx + 1}/{ len(N_list) } when N = {N}')
         exp_dict = {}
         result = {}
 
